01b81beec4a50070b0c4520250db5b3d0f3de4bc
Mustafa Yucel <yucelmustafa37@gmail.com>
2021-05-27 14:49:27 +0300

    27 mayis changes 1

--- DataAccess/Abstract/ICustomerDal.cs
+++ DataAccess/Abstract/ICustomerDal.cs
@@ -1,11 +1,12 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    interface ICustomerDal:IEntityRepository<Category>
+    public interface ICustomerDal:IEntityRepository<Category>
     {
     }
 }
