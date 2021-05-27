7c2ca095c78377d32b54c00620057a00095825b6
Mustafa Yucel <yucelmustafa37@gmail.com>
2021-05-27 11:43:26 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+
+    //interface methodları default olarak publictir. ama interface in kendisi internal olduğu için diğer katmanlardan bu interface e 
+    //erişmek için interface önüne public yazılır.
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        
+
+    }
+}
