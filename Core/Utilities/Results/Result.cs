6e04206d516e14a516df4d7004209d90f5dc4eac
Mustafa Yucel <yucelmustafa37@gmail.com>
2021-06-08 18:56:08 +0300

    8 haziran

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        
+
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        //getter lar readonly dir ve readonly ler ctor da set edilebilir.
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
